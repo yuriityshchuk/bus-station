aa552cfe74d2b5d6c745465cea6ce74478aaac57
yuriityshchuk <ytyshchuk@luxoft.com>
2020-10-18 23:04:07 +0300

    Added buy method to ticket controller

--- src/main/java/com/bus/station/busstation/service/TicketService.java
+++ src/main/java/com/bus/station/busstation/service/TicketService.java
@@ -1,20 +1,40 @@
 package com.bus.station.busstation.service;
 
 import com.bus.station.busstation.exception.DataNotFoundException;
+import com.bus.station.busstation.mapper.TicketMapper;
+import com.bus.station.busstation.model.Route;
 import com.bus.station.busstation.model.Ticket;
-import com.bus.station.busstation.model.utility.TicketStatus;
+import com.bus.station.busstation.model.User;
+import com.bus.station.busstation.model.dto.TicketDTO;
+import com.bus.station.busstation.model.utility.enums.TicketStatus;
 import com.bus.station.busstation.repository.TicketRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Date;
+
 @Service
+@Transactional
+@Log4j2
 public class TicketService {
 
     private final TicketRepository ticketRepository;
 
+    private final TicketMapper ticketMapper;
+
+    private final UserService userService;
+
+    private final RouteService routeService;
+
     @Autowired
-    public TicketService(TicketRepository ticketRepository) {
+    public TicketService(TicketRepository ticketRepository, TicketMapper ticketMapper,
+                         UserService userService, RouteService routeService) {
         this.ticketRepository = ticketRepository;
+        this.ticketMapper = ticketMapper;
+        this.userService = userService;
+        this.routeService = routeService;
     }
 
     public Ticket save(Ticket ticket) {
@@ -50,4 +70,20 @@ public class TicketService {
         return ticketRepository.getAllByUserId(userId);
     }
 
+    public void buy(TicketDTO ticketDTO, int numberOfPassengers) {
+
+        int currentNumberOfPassengers = ticketDTO.getRoute().getNumberOfPassengers();
+        User user = userService.findByUsername(ticketDTO.getUsername());
+        Route route = routeService.getById(ticketDTO.getRoute().getId());
+        route.setNumberOfPassengers(currentNumberOfPassengers - numberOfPassengers);
+        for (int i = 0; i < numberOfPassengers; i++) {
+            Ticket ticket = ticketMapper.ticket(ticketDTO);
+            ticket.setBuyTime(new Date());
+            ticket.getRoute().setNumberOfPassengers(currentNumberOfPassengers - 1);
+            ticket.setStatus(TicketStatus.ACTIVE);
+            ticket.setUser(user);
+            save(ticket);
+        }
+
+    }
 }
