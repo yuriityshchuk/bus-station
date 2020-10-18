c0e27f8a21c0f0c936575b18ed27b6a28c7d6de8
yuriityshchuk <ytyshchuk@luxoft.com>
2020-10-18 23:03:24 +0300

    Enabled aspectj Auto Proxy

--- src/main/java/com/bus/station/busstation/BusStationApplication.java
+++ src/main/java/com/bus/station/busstation/BusStationApplication.java
@@ -2,8 +2,10 @@ package com.bus.station.busstation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class BusStationApplication {
 
     public static void main(String[] args) {
