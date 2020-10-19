0873d6e383ebff754108048aa70da928958d9774
yuriityshchuk <ytyshchuk@luxoft.com>
2020-10-20 00:23:33 +0300

    Added response status to exceptions

--- src/main/java/com/bus/station/busstation/exception/CustomAuthenticationException.java
+++ src/main/java/com/bus/station/busstation/exception/CustomAuthenticationException.java
@@ -1,5 +1,9 @@
 package com.bus.station.busstation.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class CustomAuthenticationException extends RuntimeException {
 
     public CustomAuthenticationException(String message) {
--- src/main/java/com/bus/station/busstation/exception/UserException.java
+++ src/main/java/com/bus/station/busstation/exception/UserException.java
@@ -1,5 +1,9 @@
 package com.bus.station.busstation.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserException extends RuntimeException {
 
     public UserException(String message) {
